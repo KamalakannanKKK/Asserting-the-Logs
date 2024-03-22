38c6359a56d7a0fd7d5c4484cca7d6263f53ce59
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-03-22 11:40:32 +0530

    Create AssertingTheLogMessageJUnitApplication.java

---
+++ src/main/java/com/example/Asserting/the/Log/message/JUnit/AssertingTheLogMessageJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Asserting.the.Log.message.JUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssertingTheLogMessageJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssertingTheLogMessageJUnitApplication.class, args);
+	}
+
+}

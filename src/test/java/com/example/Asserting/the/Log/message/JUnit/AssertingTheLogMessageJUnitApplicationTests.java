3fe52a5405aab833b3454ffbcb2e8658a8bad90e
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-03-22 11:42:55 +0530

    Create AssertingTheLogMessageJUnitApplicationTests.java

---
+++ src/test/java/com/example/Asserting/the/Log/message/JUnit/AssertingTheLogMessageJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Asserting.the.Log.message.JUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssertingTheLogMessageJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
